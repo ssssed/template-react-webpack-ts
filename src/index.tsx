18077418baa92be2365d6b4092893d376b41d50d
Igor Koneshov <igorkoneshov@mail.ru>
2023-04-30 01:43:30 +0400

    feat: update package

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import './index.scss';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import "./index.scss";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import App from "./component/App";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
